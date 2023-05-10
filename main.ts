bf5a0aec8309318bc6915db244dd88c3e9ef347d
Gabe <martinez3gabriel@gmail.com>
2023-05-09 20:08:32 -0400

    Removed extra code

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ export default class MyPlugin extends Plugin {
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('Hello from Techfall and VS Code!');
+			new Notice('Good evening from Techfall and VS Code!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -32,16 +32,6 @@ export default class MyPlugin extends Plugin {
 
 		
 
-		
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 
 		// This is Techfall's first custom command from a custom plugin
 		this.addCommand({
@@ -51,7 +41,7 @@ export default class MyPlugin extends Plugin {
 				console.log("First console log from custom command. Great job!");
 			}
 		});
-		// This is Techfall's second custom command for a custom plugin
+		// ---------------------------------------------------------------------------- Insert Link
 		this.addCommand({
 			id: "insert-link",
 			name: "Insert link",
@@ -65,43 +55,15 @@ export default class MyPlugin extends Plugin {
 				new InsertLinkModal(this.app, selectedText, onSubmit).open();
 			}
 		});
-		// This is Techfall's third command to open a modal
+		// ---------------------------------------------------------------------------- Get Name Modal
 		this.addCommand({
-			id: "display-modal",
-			name: "Display My Modal",
+			id: "input-my-name",
+			name: "Input my name.",
 			callback: () => {
 				new ExampleModal(this.app, (result) => {
 					new Notice(`Hello, ${result}!`);
 				}).open();
 			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -127,22 +89,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
